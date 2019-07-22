1001cf0cc4c7c821c70caa6bdc0b0e4e6a92cb97
haay11 <haay03@gmail.com>
2019-07-22 10:34:16 +0900

    test2

--- app/src/main/java/com/firstapp/hytripplan/MainActivity.java
+++ app/src/main/java/com/firstapp/hytripplan/MainActivity.java
@@ -1,13 +1,17 @@
 package com.firstapp.hytripplan;
 
 import android.content.Intent;
+import android.media.Image;
 import android.os.Build;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity{
 
+
+    Image image;
     //툴바 객체 선언
     Toolbar toolbar;
     @Override
@@ -18,6 +22,11 @@ public class MainActivity extends AppCompatActivity {
         toolbar = findViewById(R.id.toolbar);
         toolbar.setTitle("Trip Plan");
         setSupportActionBar(toolbar);
+
+
+        image = findViewById(R.)
+
+
 
 
         //Compatibility checking
